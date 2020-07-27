2aad88a72a7c8637187af092fbc58aceae47d2a4
stenlyadam <stenly.adam@unklab.ac.id>
2020-07-27 16:03:46 +0800

    update firebase configuration

--- src/config/Fire/index.js
+++ src/config/Fire/index.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 firebase.initializeApp({
-  apiKey: 'AIzaSyDYF3_lObreZATItHFe8and93JsbrSuBes',
-  authDomain: 'manadopost-7bc5b.firebaseapp.com',
-  databaseURL: 'https://manadopost-7bc5b.firebaseio.com',
-  projectId: 'manadopost-7bc5b',
-  storageBucket: 'manadopost-7bc5b.appspot.com',
-  messagingSenderId: '451238088540',
-  appId: '1:451238088540:web:6d1cf8698b32a6e4643b99',
-  measurementId: 'G-L941HZ4R3W',
+  apiKey: 'AIzaSyDNWBpdNCsxaREZAElAvaxVhP5egPYjyNc',
+  authDomain: 'manadopost-f4a67.firebaseapp.com',
+  databaseURL: 'https://manadopost-f4a67.firebaseio.com',
+  projectId: 'manadopost-f4a67',
+  storageBucket: 'manadopost-f4a67.appspot.com',
+  messagingSenderId: '782626479856',
+  appId: '1:782626479856:web:67fff20a143c75847fec35',
+  measurementId: 'G-WZM2XJ00LR',
 });
 
 const Fire = firebase;

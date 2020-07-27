3a89d6cf8baa7cd8138c87a4e558357cac1c9ee2
stenlyadam <stenly.adam@unklab.ac.id>
2020-07-27 15:37:47 +0800

    change application ID

--- android/app/src/main/java/com/manadopost/MainApplication.java
+++ android/app/src/main/java/com/manadopost/MainApplication.java
@@ -1,4 +1,4 @@
-package com.manadopost_v2;
+package com.manadopost;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.manadopost_v2.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.manadopost.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);

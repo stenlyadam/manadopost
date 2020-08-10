8a30970184cc04362b9833ac9147751719cc1fd1
stenlyadam <stenly.adam@unklab.ac.id>
2020-08-10 16:06:11 +0800

    change epaper & magazine url to wordpress

--- src/screens/Digital/index.js
+++ src/screens/Digital/index.js
@@ -1,35 +1,93 @@
-import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
-import {Header, MagazineCard, Title} from '../../components';
-import {Fire} from '../../config';
-import {colors} from '../../utils';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import Axios from 'axios';
+import Moment from 'moment';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {Header, MagazineCard, Notification, Title} from '../../components';
+import {colors} from '../../utils';
 
 const Digital = ({route, navigation}) => {
-  const {title} = route.params;
-  const [magazine, setMagazine] = useState([]);
+  const [epaper, setEpaper] = useState([]);
   const [show, setShow] = useState(false);
+  const {title} = route.params;
+  const [refreshing, setRefreshing] = useState(false);
+  const login = useSelector((state) => state.login);
 
-  useEffect(() => {
-    Fire.database()
-      .ref('magazines/')
-      .once('value')
-      .then((res) => {
-        if (res.val()) {
-          setMagazine(res.val());
-        }
+  const getEPaper = async () => {
+    let url = 'http://api.mpdigital.id/mp';
+    const response = await Axios.get(url);
+    let filteredData = response.data.filter((el) => {
+      return el.x_type === 'Digital';
+    });
+    return filteredData;
+  };
+
+  const onChangeDate = async (event, selectedDate) => {
+    getEPaper().then((res) => {
+      let data = res.filter((el) => {
+        return (
+          Moment(el.publish_date).format('LL') ===
+          Moment(selectedDate).format('LL')
+        );
       });
-  }, []);
-
-  const onChangeDate = (event, selectedDate) => {
-    console.log('event, ', event);
-    console.log('selected Date, ', selectedDate);
+      setEpaper(data);
+    });
     setShow(false);
   };
 
   const onPress = () => {
     setShow(true);
   };
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.content}>
+        <MagazineCard
+          key={item.id}
+          image={{uri: item.thumbnail}}
+          date={Moment(item.publish_date).format('LL')}
+          onPress={() =>
+            navigation.navigate('ReadMagazine', {
+              uri: item.pdf_link,
+              title: title,
+            })
+          }
+        />
+      </View>
+    );
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    getEPaper().then((res) => {
+      res.reverse();
+      setEpaper(res);
+      setRefreshing(false);
+    });
+  };
+
+  useEffect(() => {
+    getEPaper().then((res) => {
+      res.reverse();
+      setEpaper(res);
+    });
+  }, []);
+
+  if (!login) {
+    return (
+      <View style={styles.screen}>
+        <Header
+          type="logo-profile"
+          onPressUserProfile={() => navigation.navigate('UserProfile')}
+        />
+        <Title title={title} />
+        <Notification
+          title={`Silahkan login terlebih dahulu untuk dapat mengakses menu ${title}`}
+        />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.screen}>
@@ -46,24 +104,15 @@ const Digital = ({route, navigation}) => {
           onChange={onChangeDate}
         />
       )}
-
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.content}>
-          {magazine.map((item) => (
-            <MagazineCard
-              key={item.id}
-              image={{uri: item.thumbnail}}
-              date={item.date}
-              onPress={() =>
-                navigation.navigate('ReadMagazine', {
-                  uri: item.link,
-                  title: title,
-                })
-              }
-            />
-          ))}
-        </View>
-      </ScrollView>
+      <FlatList
+        data={epaper.slice(0, 20)}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+        numColumns={2}
+      />
     </View>
   );
 };
@@ -76,8 +125,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
+    padding: 20,
   },
 });
